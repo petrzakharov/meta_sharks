6a129f3b367770b6e1ad5bb090f3ac63fe546f49
Peter Zakharov <47098349+petrzakharov@users.noreply.github.com>
2022-10-20 22:52:31 +0400

    Update settings.py

--- conf/settings.py
+++ conf/settings.py
@@ -53,12 +53,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'conf.wsgi.application'
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 DATABASES = {
     'default': {
